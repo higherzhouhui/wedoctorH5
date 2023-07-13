21d1da0a3aa9a1cd9a6c833539e8331cdc60848d
higherzhouhui <782492184@qq.com>
2023-07-14 01:19:25 +0800

    f

--- config/index.js
+++ config/index.js
@@ -2,6 +2,6 @@
  const LINK = "http://web.jianxiangyunbao.cc"
 // 请求的基本路径
 const URL = "http://47.96.151.84:8900"
-const baseURL = window ? '/api' : URL
+const baseURL = process.env.NODE_ENV === "development" ? '/api' : URL
  
 export {baseURL, URL, LINK}
